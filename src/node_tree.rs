3e14f903e1265c5bd195b2c7e74170de28495dd5
Alice Lim <strelka@connect.hku.hk>
2022-08-05 05:30:57 +0800

    DOCUMENTATION: important comment about the behavior of node_tree::insert_nodes.

--- src/node_tree.rs
+++ src/node_tree.rs
@@ -8,6 +8,8 @@ pub struct NodeTree {
 
 impl NodeTree {
     fn open() -> NodeTree { todo!() } 
+
+    // In the case nodes[0].qc.node_hash does not refer to an existing node, this is a no-op.
     fn insert_nodes(&mut self, nodes: &[types::ExecutedNode]) { todo!() }
     fn get_locked_qc(&self) -> types::QuorumCertificate { todo!() }
     fn get_prepare_qc(&self) -> types::QuorumCertificate { todo!() }
