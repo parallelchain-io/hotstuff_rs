4e9af1e7b54c76268f392aa85578a3cdee566286
Alice Lim <strelka@connect.hku.hk>
2022-08-18 04:54:18 +0800

    DOCUMENTATION: notes about implementation of ipc::handle and node_tree::api.

--- src/ipc/handle.rs
+++ src/ipc/handle.rs
@@ -1,5 +1,6 @@
 use std::net::SocketAddr;
 use std::sync::{Arc, RwLock, mpsc};
+use std::time::Duration;
 use crate::msg_types::{ConsensusMsg, PublicAddress};
 use crate::ipc::ConnectionSet;
 use crate::ipc::manager::{EstablisherRequest, SendRequest};
@@ -24,11 +25,20 @@ impl Handle {
         self.to_sender.send(SendRequest::Broadcast(msg)).unwrap();
     }
 
-    pub fn recv_from(&self, participant: PublicAddress) -> ConsensusMsg {
+    /// timeout = max(timeout, READ_TIMEOUT).
+    pub fn recv_from(&self, participant: PublicAddress, timeout: Option<Duration>) -> ConsensusMsg {
         todo!()
     }
 
-    fn recv_from_any() -> ConsensusMsg {
+    fn recv_from_any(timeout: Option<Duration>) -> ConsensusMsg {
+        // Rough flow:
+        // 1. In a threadpool, spawn as many worker threads as there are open connections.
+        // 2. Worker threads try to peek from each connection. If it manages to get a ConsensusMsg, it sends it
+        //    back to the main thread.
+        // Alt. If an undeserializable message appears or the connection fails in any way, send a Reconnect request
+        //      to the Establisher.
+        // 3. The moment the main thread receives a ConsensusMsg, remove that message from the socket on which in was
+        //    received, and then return the ConsensusMsg.
         todo!()
     }
 }
--- src/node_tree/api.rs
+++ src/node_tree/api.rs
@@ -1,3 +1,11 @@
 struct Server;
 
 struct SyncModeClient;
+
+// ## Route
+// `GET /nodes`
+//
+// ## Params
+// 
+// 1. EITHER `hash` OR `height`:
+// 2. `limit`
