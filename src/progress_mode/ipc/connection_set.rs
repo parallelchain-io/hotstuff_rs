bd86bb14558d9845c0be22e6f119e252c1a7748a
Alice Lim <strelka@connect.hku.hk>
2022-08-22 13:21:40 +0800

    REFACTOR: replace channels between Maintainer and the Initiator and Listener threads with a shared Tasks list, which feels like a more intuitive and simple interaction model.

--- src/progress_mode/ipc/connection_set.rs
+++ src/progress_mode/ipc/connection_set.rs
@@ -46,34 +46,30 @@ impl ConnectionSet {
         cmds: mpsc::Receiver<MaintainerCmd>,
     ) -> thread::JoinHandle<()> { 
         // 1. Get MaintainerCmd.
-        // 2. If Replace, split into two EstablisherCmds using the predicates described in the comments below: one for initiator, one for listener.
-        // 3. established conn = recv_timeout from initiator and from listener. if conn is in target_conns but not in connections, put it in
-        //    connections.
+        // 2. If Replace, set target_conns = new_conns. Then, compute new_conns - cur_conns = tasks. Split tasks into two task lists according to the predicates in the comments below.
+        // 3. established conn = recv_timeout from initiator and from listener. If conns is not in target_conns, discard. Else, assert that it is *not* in conns, then insert to conns.
+        // 4. If Reconnect, then put in the appropriate task list (according to the predicates).
         todo!()
     } 
 
-    /// iff target_public_address < my_public_address, to_initiator.send...
+    /// iff target_public_address < my_public_address, initiator_tasks.insert...
     fn initiator(
-        cmds: mpsc::Receiver<EstablisherCmd>,
+        tasks: Arc<Mutex<IndexMap<PublicAddress, SocketAddr>>>,
         established_conns: mpsc::Sender<(PublicAddress, ipc::Stream)>,
     ) -> thread::JoinHandle<()> { 
-        // 1. Get EstablisherCmd.
-        //     - If ReplaceTasks, replace tasks with the new one. 
-        //     - If AddTask, add new task. 
-        // 2. Pick random tasks, start doing it.
-        // 3. If successful, remove from tasks and send established conn to maintainer.
+        // 1. Pick random tasks, try to establish connection.
+        // 2. If successful, remove from tasks and send established conn to maintainer.
         todo!()
     }
 
-    /// else, to_listener.send...
+    /// iff target_public_address > my_public_address, listener_tasks.insert...
     fn listener(
-        cmds: mpsc::Receiver<EstablisherCmd>,
+        tasks: Arc<Mutex<IndexMap<PublicAddress, SocketAddr>>>,
         established_conns: mpsc::Sender<(PublicAddress, ipc::Stream)>,
     ) -> thread::JoinHandle<()> { 
-        // 1. Same as initiator.1.
-        // 2. Listen to serversocket up to a timeout.
-        // 3. Check if initiated stream is in tasklist if so, initiate connection. 
-        // 4. If successful, remove from tasks and send established conn to maintainer.
+        // 1. Listen for new streams.
+        // 2. Check if new stream is in tasks. If so, initiate connection. 
+        // 3. If successful, remove from tasks and send established conn to maintainer.
         todo!()
     }
 }
@@ -81,9 +77,4 @@ impl ConnectionSet {
 enum MaintainerCmd {
     Replace(ParticipantSet),
     Reconnect(PublicAddress),
-}
-
-enum EstablisherCmd {
-    ReplaceTasks(ParticipantSet),
-    AddTask(PublicAddress),
 }
