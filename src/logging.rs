2fbb95b7f7058d48449a1946622992121bbca4ff
Alice Lim <strelka@connect.hku.hk>
2023-11-30 15:36:20 +0800

    Log SystemTime as the number of seconds since the unix epoch.

--- src/logging.rs
+++ src/logging.rs
@@ -3,56 +3,63 @@
     Licensed under the Apache License, Version 2.0: http://www.apache.org/licenses/LICENSE-2.0
 */
 
-//! Functions that log out events [events](crate::events).
+//! Functions that log out [events](crate::events).
 //!
 //! The logs defined in this module are printed if the user enabled them via replica's [config](crate::replica::Configuration).
 //! 
-//! HotStuff-rs logs using the [log](https://docs.rs/log/latest/log/) crate. To get these messages
-//! printed onto a terminal or to a file, set up a [logging
-//! implementation](https://docs.rs/log/latest/log/#available-logging-implementations).
+//! HotStuff-rs logs using the [log](https://docs.rs/log/latest/log/) crate. To get these messages printed onto a terminal or
+//! to a file, set up a [logging implementation](https://docs.rs/log/latest/log/#available-logging-implementations).
 //! 
 //! ## Log message format
 //! 
-//! The log messages follow a CSV-like formatting style, with the following information separated by commas:
-//! 1. Event name,
-//! 2. Timestamp,
-//! 3. Public key (if any),
-//! 4. Block hash (if any),
-//! 5. Block height (if any),
-//! 6. View (if any),
-//! 7. Phase (if any),
-//! 8. Other information (if any).
+//! Log messages are CSVs (Comma Separated Values) with at least two values. The first two values are always:
+//! 1. The name of the event in PascalCase.
+//! 2. The time the event was emitted (as number of seconds since the Unix Epoch).
+//! 
+//! The rest of the values differ depending on the kind of event. For example, the following snippet is how a 
+//! [ReceiveProposal](crate::events::ReceiveProposalEvent) is printed:
+//! 
+//! ```
+//! ReceiveProposal, 1701329264, Id5u7f6, fNGCJyk, 0
+//! ```
+//! 
+//! In the snippet:
+//! - The third value is the first seven characters of the Base64 encoding of the public address of the origin of the proposal.
+//! - The fourth value is the first seven characters of the Base64 encoding of the hash of the proposed block.
+//! - The fifth value is the height of the proposed block.
+
+use std::time::SystemTime;
 
 use base64::{engine::general_purpose::STANDARD_NO_PAD, Engine as _};
 use log;
 
 pub(crate) use crate::events::*;
 
-pub const INSERT_BLOCK: &str = "InsertedBlock";
-pub const COMMIT_BLOCK: &str = "CommittedBlock";
-pub const PRUNE_BLOCK: &str = "PrunedBlock";
-pub const UPDATE_HIGHEST_QC: &str = "UpdatedHighestQC";
-pub const UPDATE_LOCKED_VIEW: &str = "UpdatedLockedView";
-pub const UPDATE_VALIDATOR_SET: &str = "UpdatedValidatorSet";
+pub const INSERT_BLOCK: &str = "InsertBlock";
+pub const COMMIT_BLOCK: &str = "CommitBlock";
+pub const PRUNE_BLOCK: &str = "PruneBlock";
+pub const UPDATE_HIGHEST_QC: &str = "UpdateHighestQC";
+pub const UPDATE_LOCKED_VIEW: &str = "UpdateLockedView";
+pub const UPDATE_VALIDATOR_SET: &str = "UpdateValidatorSet";
 
-pub const PROPOSE: &str = "Proposed";
-pub const NUDGE: &str = "Nudged";
-pub const VOTE: &str = "Voted";
-pub const NEWVIEW: &str = "NewView";
+pub const PROPOSE: &str = "Propose";
+pub const NUDGE: &str = "Nudge";
+pub const VOTE: &str = "Vote";
+pub const NEW_VIEW: &str = "NewView";
 
-pub const RECEIVED_PROPOSAL: &str = "ReceivedProposal";
-pub const RECEIVED_NUDGE: &str = "ReceivedNudge";
-pub const RECEIVED_VOTE: &str = "ReceivedVote";
-pub const RECEIVED_NEW_VIEW: &str = "ReceivedNewView";
+pub const RECEIVE_PROPOSAL: &str = "ReceiveProposal";
+pub const RECEIVE_NUDGE: &str = "ReceiveNudge";
+pub const RECEIVE_VOTE: &str = "ReceiveVote";
+pub const RECEIVE_NEW_VIEW: &str = "ReceiveNewView";
 
-pub const START_VIEW: &str = "EnteredView";
-pub const VIEW_TIME_OUT: &str = "ViewTimedOut";
-pub const COLLECT_QC: &str = "CollectedQC";
+pub const START_VIEW: &str = "StartView";
+pub const VIEW_TIMEOUT: &str = "ViewTimeout";
+pub const COLLECT_QC: &str = "CollectQC";
 
-pub const START_SYNC: &str = "StartedSyncing";
-pub const END_SYNC: &str = "FinishedSyncing";
-pub const RECEIVE_SYNC_REQUEST: &str = "ReceivedSyncRequest";
-pub const SEND_SYNC_RESPONSE: &str = "SentSyncResponse";
+pub const START_SYNC: &str = "StartSync";
+pub const END_SYNC: &str = "EndSync";
+pub const RECEIVE_SYNC_REQUEST: &str = "ReceiveSyncRequest";
+pub const SEND_SYNC_RESPONSE: &str = "SendSyncResponse";
 
 /// Abstraction for event types that have default logging handlers defined.
 pub(crate) trait Logger {
@@ -64,10 +71,10 @@ impl Logger for InsertBlockEvent {
     fn get_logger() -> Box<dyn Fn(&Self) + Send> {
         let logger = |insert_block_event: &InsertBlockEvent| {
             log::info!(
-                "{}, {:?}, {}, {}",
+                "{}, {}, {}, {}",
                 INSERT_BLOCK,
-                insert_block_event.timestamp,
-                succinct(&insert_block_event.block.hash),
+                secs_since_unix_epoch(insert_block_event.timestamp),
+                first_seven_base64_chars(&insert_block_event.block.hash),
                 insert_block_event.block.height
             )
         };
@@ -78,7 +85,7 @@ impl Logger for InsertBlockEvent {
 impl Logger for CommitBlockEvent {
     fn get_logger() -> Box<dyn Fn(&Self) + Send> {
         let logger = |commit_block_event: &CommitBlockEvent| {
-            log::info!("{}, {:?}, {}", COMMIT_BLOCK, commit_block_event.timestamp, succinct(&commit_block_event.block))
+            log::info!("{}, {}, {}", COMMIT_BLOCK, secs_since_unix_epoch(commit_block_event.timestamp), first_seven_base64_chars(&commit_block_event.block))
         };
         Box::new(logger)
     }
@@ -87,7 +94,7 @@ impl Logger for CommitBlockEvent {
 impl Logger for PruneBlockEvent {
     fn get_logger() -> Box<dyn Fn(&Self) + Send> {
         let logger = |prune_block_event: &PruneBlockEvent| {
-            log::info!("{}, {:?}, {}", PRUNE_BLOCK, prune_block_event.timestamp, succinct(&prune_block_event.block))
+            log::info!("{}, {}, {}", PRUNE_BLOCK, secs_since_unix_epoch(prune_block_event.timestamp), first_seven_base64_chars(&prune_block_event.block))
         };
         Box::new(logger)
     }
@@ -97,10 +104,10 @@ impl Logger for UpdateHighestQCEvent {
     fn get_logger() -> Box<dyn Fn(&Self) + Send> {
         let logger = |update_highest_qc_event: &UpdateHighestQCEvent| {
             log::info!(
-                "{}, {:?}, {}, {}, {:?}",
+                "{}, {}, {}, {}, {:?}",
                 UPDATE_HIGHEST_QC,
-                update_highest_qc_event.timestamp,
-                succinct(&update_highest_qc_event.highest_qc.block),
+                secs_since_unix_epoch(update_highest_qc_event.timestamp),
+                first_seven_base64_chars(&update_highest_qc_event.highest_qc.block),
                 update_highest_qc_event.highest_qc.view,
                 update_highest_qc_event.highest_qc.phase
             )
@@ -113,9 +120,9 @@ impl Logger for UpdateLockedViewEvent {
     fn get_logger() -> Box<dyn Fn(&Self) + Send> {
         let logger = |update_locked_view_event: &UpdateLockedViewEvent| {
             log::info!(
-                "{}, {:?}, {}",
+                "{}, {}, {}",
                 UPDATE_LOCKED_VIEW,
-                update_locked_view_event.timestamp,
+                secs_since_unix_epoch(update_locked_view_event.timestamp),
                 update_locked_view_event.locked_view
             )
         };
@@ -127,10 +134,10 @@ impl Logger for UpdateValidatorSetEvent {
     fn get_logger() -> Box<dyn Fn(&Self) + Send> {
         let logger = |update_validator_set_event: &UpdateValidatorSetEvent| {
             log::info!(
-                "{}, {:?}, {}",
+                "{}, {}, {}",
                 UPDATE_VALIDATOR_SET,
-                update_validator_set_event.timestamp,
-                succinct(&update_validator_set_event.cause_block)
+                secs_since_unix_epoch(update_validator_set_event.timestamp),
+                first_seven_base64_chars(&update_validator_set_event.cause_block)
             )
         };
         Box::new(logger)
@@ -141,10 +148,10 @@ impl Logger for ProposeEvent {
     fn get_logger() -> Box<dyn Fn(&Self) + Send> {
         let logger = |propose_event: &ProposeEvent| {
             log::info!(
-                "{}, {:?}, {}, {}, {}",
+                "{}, {}, {}, {}, {}",
                 PROPOSE,
-                propose_event.timestamp,
-                succinct(&propose_event.proposal.block.hash),
+                secs_since_unix_epoch(propose_event.timestamp),
+                first_seven_base64_chars(&propose_event.proposal.block.hash),
                 propose_event.proposal.block.height,
                 propose_event.proposal.view
             )
@@ -157,10 +164,10 @@ impl Logger for NudgeEvent {
     fn get_logger() -> Box<dyn Fn(&Self) + Send> {
         let logger = |nudge_event: &NudgeEvent| {
             log::info!(
-                "{}, {:?}, {}, {}, {:?}",
+                "{}, {}, {}, {}, {:?}",
                 NUDGE,
-                nudge_event.timestamp,
-                succinct(&nudge_event.nudge.justify.block),
+                secs_since_unix_epoch(nudge_event.timestamp),
+                first_seven_base64_chars(&nudge_event.nudge.justify.block),
                 nudge_event.nudge.view,
                 nudge_event.nudge.justify.phase
             )
@@ -173,10 +180,10 @@ impl Logger for VoteEvent {
     fn get_logger() -> Box<dyn Fn(&Self) + Send> {
         let logger = |vote_event: &VoteEvent| {
             log::info!(
-                "{}, {:?}, {}, {}, {:?}",
+                "{}, {}, {}, {}, {:?}",
                 VOTE,
-                vote_event.timestamp,
-                succinct(&vote_event.vote.block),
+                secs_since_unix_epoch(vote_event.timestamp),
+                first_seven_base64_chars(&vote_event.vote.block),
                 vote_event.vote.view,
                 vote_event.vote.phase
             )
@@ -189,10 +196,10 @@ impl Logger for NewViewEvent {
     fn get_logger() -> Box<dyn Fn(&Self) + Send> {
         let logger = |new_view_event: &NewViewEvent| {
             log::info!(
-                "{}, {:?}, {}, {}, {:?}",
-                NEWVIEW,
-                new_view_event.timestamp,
-                succinct(&new_view_event.new_view.highest_qc.block),
+                "{}, {}, {}, {}, {:?}",
+                NEW_VIEW,
+                secs_since_unix_epoch(new_view_event.timestamp),
+                first_seven_base64_chars(&new_view_event.new_view.highest_qc.block),
                 new_view_event.new_view.view,
                 new_view_event.new_view.highest_qc.phase
             )
@@ -205,11 +212,11 @@ impl Logger for ReceiveProposalEvent {
     fn get_logger() -> Box<dyn Fn(&Self) + Send> {
         let logger = |receive_proposal_event: &ReceiveProposalEvent| {
             log::info!(
-                "{}, {:?}, {}, {}, {}",
-                RECEIVED_PROPOSAL,
-                receive_proposal_event.timestamp,
-                succinct(&receive_proposal_event.origin.to_bytes()),
-                succinct(&receive_proposal_event.proposal.block.hash),
+                "{}, {}, {}, {}, {}",
+                RECEIVE_PROPOSAL,
+                secs_since_unix_epoch(receive_proposal_event.timestamp),
+                first_seven_base64_chars(&receive_proposal_event.origin.to_bytes()),
+                first_seven_base64_chars(&receive_proposal_event.proposal.block.hash),
                 receive_proposal_event.proposal.block.height
             )
         };
@@ -221,11 +228,11 @@ impl Logger for ReceiveNudgeEvent {
     fn get_logger() -> Box<dyn Fn(&Self) + Send> {
         let logger = |receive_nudge_event: &ReceiveNudgeEvent| {
             log::info!(
-                "{}, {:?}, {}, {}, {:?}",
-                RECEIVED_NUDGE,
-                receive_nudge_event.timestamp,
-                succinct(&receive_nudge_event.origin.to_bytes()),
-                succinct(&receive_nudge_event.nudge.justify.block),
+                "{}, {}, {}, {}, {:?}",
+                RECEIVE_NUDGE,
+                secs_since_unix_epoch(receive_nudge_event.timestamp),
+                first_seven_base64_chars(&receive_nudge_event.origin.to_bytes()),
+                first_seven_base64_chars(&receive_nudge_event.nudge.justify.block),
                 receive_nudge_event.nudge.justify.phase
             )
         };
@@ -237,11 +244,11 @@ impl Logger for ReceiveVoteEvent {
     fn get_logger() -> Box<dyn Fn(&Self) + Send> {
         let logger = |receive_vote_event: &ReceiveVoteEvent| {
             log::info!(
-                "{}, {:?}, {}, {}, {:?}",
-                RECEIVED_VOTE,
-                receive_vote_event.timestamp,
-                succinct(&receive_vote_event.origin.to_bytes()),
-                succinct(&receive_vote_event.vote.block),
+                "{}, {}, {}, {}, {:?}",
+                RECEIVE_VOTE,
+                secs_since_unix_epoch(receive_vote_event.timestamp),
+                first_seven_base64_chars(&receive_vote_event.origin.to_bytes()),
+                first_seven_base64_chars(&receive_vote_event.vote.block),
                 receive_vote_event.vote.phase
             )
         };
@@ -253,11 +260,11 @@ impl Logger for ReceiveNewViewEvent {
     fn get_logger() -> Box<dyn Fn(&Self) + Send> {
         let logger = |receive_new_view_event: &ReceiveNewViewEvent| {
             log::info!(
-                "{}, {:?}, {}, {}, {}, {:?}",
-                RECEIVED_NEW_VIEW,
-                receive_new_view_event.timestamp,
-                succinct(&receive_new_view_event.origin.to_bytes()),
-                succinct(&receive_new_view_event.new_view.highest_qc.block),
+                "{}, {}, {}, {}, {}, {:?}",
+                RECEIVE_NEW_VIEW,
+                secs_since_unix_epoch(receive_new_view_event.timestamp),
+                first_seven_base64_chars(&receive_new_view_event.origin.to_bytes()),
+                first_seven_base64_chars(&receive_new_view_event.new_view.highest_qc.block),
                 receive_new_view_event.new_view.view,
                 receive_new_view_event.new_view.highest_qc.phase
             )
@@ -270,10 +277,10 @@ impl Logger for StartViewEvent {
     fn get_logger() -> Box<dyn Fn(&Self) + Send> {
         let logger = |start_view_event: &StartViewEvent| {
             log::info!(
-                "{}, {:?}, {}, {}",
+                "{}, {}, {}, {}",
                 START_VIEW,
-                start_view_event.timestamp,
-                succinct(&start_view_event.leader.to_bytes()),
+                secs_since_unix_epoch(start_view_event.timestamp),
+                first_seven_base64_chars(&start_view_event.leader.to_bytes()),
                 start_view_event.view
             )
         };
@@ -285,9 +292,9 @@ impl Logger for ViewTimeoutEvent {
     fn get_logger() -> Box<dyn Fn(&Self) + Send> {
         let logger = |view_timeout_event: &ViewTimeoutEvent| {
             log::info!(
-                "{}, {:?}, {}, {:?}",
-                VIEW_TIME_OUT,
-                view_timeout_event.timestamp,
+                "{}, {}, {}, {:?}",
+                VIEW_TIMEOUT,
+                secs_since_unix_epoch(view_timeout_event.timestamp),
                 view_timeout_event.view,
                 view_timeout_event.timeout,
             )
@@ -300,10 +307,10 @@ impl Logger for CollectQCEvent {
     fn get_logger() -> Box<dyn Fn(&Self) + Send> {
         let logger = |collect_qc_event: &CollectQCEvent| {
             log::info!(
-                "{}, {:?}, {}, {}, {:?}",
+                "{}, {}, {}, {}, {:?}",
                 COLLECT_QC,
-                collect_qc_event.timestamp,
-                succinct(&collect_qc_event.quorum_certificate.block),
+                secs_since_unix_epoch(collect_qc_event.timestamp),
+                first_seven_base64_chars(&collect_qc_event.quorum_certificate.block),
                 collect_qc_event.quorum_certificate.view,
                 collect_qc_event.quorum_certificate.phase,
             )
@@ -315,7 +322,7 @@ impl Logger for CollectQCEvent {
 impl Logger for StartSyncEvent {
     fn get_logger() -> Box<dyn Fn(&Self) + Send> {
         let logger = |start_sync_event: &StartSyncEvent| {
-            log::info!("{}, {:?}, {}", START_SYNC, start_sync_event.timestamp, succinct(&start_sync_event.peer.to_bytes()))
+            log::info!("{}, {}, {}", START_SYNC, secs_since_unix_epoch(start_sync_event.timestamp), first_seven_base64_chars(&start_sync_event.peer.to_bytes()))
         };
         Box::new(logger)
     }
@@ -324,7 +331,7 @@ impl Logger for StartSyncEvent {
 impl Logger for EndSyncEvent {
     fn get_logger() -> Box<dyn Fn(&Self) + Send> {
         let logger = |end_sync_event: &EndSyncEvent| {
-            log::info!("{}, {:?}, {}, {}", END_SYNC, end_sync_event.timestamp, succinct(&end_sync_event.peer.to_bytes()), end_sync_event.blocks_synced)
+            log::info!("{}, {}, {}, {}", END_SYNC, secs_since_unix_epoch(end_sync_event.timestamp), first_seven_base64_chars(&end_sync_event.peer.to_bytes()), end_sync_event.blocks_synced)
         };
         Box::new(logger)
     }
@@ -334,10 +341,10 @@ impl Logger for ReceiveSyncRequestEvent {
     fn get_logger() -> Box<dyn Fn(&Self) + Send> {
         let logger = |receive_sync_request_event: &ReceiveSyncRequestEvent| {
             log::info!(
-                "{}, {:?}, {}, {}, {}",
+                "{}, {}, {}, {}, {}",
                 RECEIVE_SYNC_REQUEST,
-                receive_sync_request_event.timestamp,
-                succinct(&receive_sync_request_event.peer.to_bytes()),
+                secs_since_unix_epoch(receive_sync_request_event.timestamp),
+                first_seven_base64_chars(&receive_sync_request_event.peer.to_bytes()),
                 receive_sync_request_event.start_height,
                 receive_sync_request_event.limit
             )  
@@ -350,11 +357,11 @@ impl Logger for SendSyncResponseEvent {
     fn get_logger() -> Box<dyn Fn(&Self) + Send> {
         let logger = |send_sync_response_event: &SendSyncResponseEvent| {
             log::info!(
-                "{}, {:?}, {}, {}, {}",
+                "{}, {}, {}, {}, {}",
                 SEND_SYNC_RESPONSE,
-                send_sync_response_event.timestamp,
-                succinct(&send_sync_response_event.peer.to_bytes()),
-                succinct(&send_sync_response_event.highest_qc.block),
+                secs_since_unix_epoch(send_sync_response_event.timestamp),
+                first_seven_base64_chars(&send_sync_response_event.peer.to_bytes()),
+                first_seven_base64_chars(&send_sync_response_event.highest_qc.block),
                 send_sync_response_event.blocks.len(),
             )  
         };
@@ -363,13 +370,17 @@ impl Logger for SendSyncResponseEvent {
 }
 
 // Get a more readable representation of a bytesequence by base64-encoding it and taking the first 7 characters.
-pub(crate) fn succinct(bytes: &[u8]) -> String {
+fn first_seven_base64_chars(bytes: &[u8]) -> String {
     let encoded = STANDARD_NO_PAD.encode(bytes);
     if encoded.len() > 7 {
-        let mut truncated = encoded[0..7].to_string();
-        truncated.push_str("..");
-        truncated
+        encoded[0..7].to_string()
     } else {
         encoded
     }
+}
+
+fn secs_since_unix_epoch(timestamp: SystemTime) -> u64 {
+    timestamp.duration_since(SystemTime::UNIX_EPOCH)
+        .expect("Event occured before the Unix Epoch.")
+        .as_secs()
 }
